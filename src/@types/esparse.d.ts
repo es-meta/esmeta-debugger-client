18159c7181fd93b7e747322dc40ee1c6c9d3f099
h2oche <h2oche22@gmail.com>
2022-03-22 18:50:11 +0900

    Removed esparse

--- src/@types/esparse.d.ts
+++
@@ -1,7 +0,0 @@
-// esparse type definition
-declare class ESParse {
-  // constructor
-  constructor(version: string);
-  // return stringified, compressed form of parsing result from translator
-  parseWithCompress(code): string;
-}
