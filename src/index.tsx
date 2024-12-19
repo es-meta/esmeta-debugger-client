e4f06f778d06a80adc0935df31302e0c72e18a5a
송경호 <s07019@naver.com>
2024-12-20 02:48:29 +0900

    Add react router

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,28 @@
 import React from "react";
-import { createRoot } from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 import "./styles/index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import CssBaseline from "@mui/material/CssBaseline";
-import 'react-grid-layout/css/styles.css';
-import 'react-resizable/css/styles.css';
+import "react-grid-layout/css/styles.css";
+import "react-resizable/css/styles.css";
 import store from "./store";
 import { Provider } from "react-redux";
+import { BrowserRouter, Route, Routes } from "react-router";
 
-const root = createRoot(document.getElementById('root')!);
+const root = createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
     <CssBaseline />
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/doc" element={<div>Document</div>} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
