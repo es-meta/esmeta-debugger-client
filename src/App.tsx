c676664b54a70dd721def15792cc9e0ccbe9dff5
송경호 <s07019@naver.com>
2024-12-20 03:47:20 +0900

    Connect with ecma-visualizer

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,22 @@
 import React from "react";
-import { Grid, ThemeProvider, createTheme } from "@mui/material";
-import { makeStyles } from "@mui/styles";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-import { connect, ConnectedProps } from "react-redux";
-import { ReduxState, Dispatch } from "./store";
-import { AppState } from "./store/reducers/AppState";
-import { updateAlgoListRequest } from "./store/reducers/Spec";
 import Header from "@/components/custom/Header";
 import Footer from "@/components/custom/Footer";
 import DebuggerApp from "@/features/DebuggerApp";
 
+const App = () => {
+  return (
+    <div className="min-h-dvh bg-neutral-100">
+      <ToastContainer autoClose={3000} hideProgressBar={true} />
+      <Header />
+      <div className="relative">
+        <DebuggerApp />
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
-const theme = createTheme();
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const useStyles = makeStyles((theme : unknown) => {
-root: {
-    // some CSS that accesses the theme
-  }
-});
-
-// connect redux store
-const mapStateToProps = (st: ReduxState) => ({
-  appState: st.appState.state,
-});
-const mapDispatchToProps = (dispatch: Dispatch) => ({
-  updateAlgoListRequest: () => dispatch(updateAlgoListRequest()),
-});
-const connector = connect(mapStateToProps, mapDispatchToProps);
-type AppProps = ConnectedProps<typeof connector>;
-
-// App component
-class App extends React.Component<AppProps> {
-
-  render() {
-    return (
-      <ThemeProvider theme={theme}>
-        <div className="min-h-dvh bg-neutral-100">
-          <ToastContainer autoClose={3000} hideProgressBar={true} />
-          <Header />
-          <div className="relative">
-            <DebuggerApp />
-          </div>
-          <Footer />
-        </div>
-      </ThemeProvider>
-    );
-  }
-}
-
-export default connector(App);
+export default App;
--- src/features/DebuggerApp.tsx
+++ src/features/DebuggerApp.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { Grid } from "@mui/material";
+import React, { useEffect } from "react";
 
 import SpecViewer from "./SpecViewer";
 import Toolbar from "./toolbar/Toolbar";
@@ -11,45 +10,96 @@ import { ReduxState, Dispatch } from "@/store";
 import { AppState } from "@/store/reducers/AppState";
 import { updateAlgoListRequest } from "@/store/reducers/Spec";
 import ConnectState from "./ConnectState";
+import { edit } from "@/store/reducers/JS";
+import { run, specContinue } from "@/store/reducers/Debugger";
+import {
+  addBreak,
+  Breakpoint,
+  BreakpointType,
+} from "@/store/reducers/Breakpoint";
+import { useSearchParams } from "react-router";
+import { parseStep } from "@/util/convert";
+import { toast } from "react-toastify";
 
 // connect redux store
 const mapStateToProps = (st: ReduxState) => ({
   appState: st.appState.state,
+  breakpoints: st.breakpoint.items,
+  algos: st.spec.nameMap,
 });
 const mapDispatchToProps = (dispatch: Dispatch) => ({
   updateAlgoListRequest: () => dispatch(updateAlgoListRequest()),
+  edit: (code: string) => dispatch(edit(code)),
+  specContinue: () => dispatch(specContinue()),
+  run: () => dispatch(run()),
+  addBreak: (bp: Breakpoint) => dispatch(addBreak(bp)),
 });
+
 const connector = connect(mapStateToProps, mapDispatchToProps);
 type AppProps = ConnectedProps<typeof connector>;
 
 // App component
-class DebuggerApp extends React.Component<AppProps> {
-  componentDidMount() {
-    if (this.props.appState === AppState.INIT)
-      this.props.updateAlgoListRequest();
-  }
+const DebuggerApp = ({
+  appState,
+  algos,
+  breakpoints,
+  ...methods
+}: AppProps) => {
+  const [searchParams] = useSearchParams();
+  const program = searchParams.get("program");
+  const step = searchParams.get("stepId")?.split("/")[1];
+  const algoName = searchParams.get("algoName");
 
-  render() {
-    return (
-      <>
-        <ConnectState />
-        <Toolbar />
-        
-      <Grid container spacing={2} columns={{ xs: 6, sm: 12, md: 18, lg: 18 }}
-        className="w-full justify-center items-start relative lg:px-24">
-        <Grid item xs={6}>
-        <JSEditor />
-        </Grid>
-        <Grid item xs={6}>
-        <SpecViewer />
-        </Grid>
-        <Grid item xs={6}>
-        <StateViewer />
-        </Grid>
-        </Grid>
-      </>
+  useEffect(() => {
+    if (appState !== AppState.INIT) return;
+    methods.updateAlgoListRequest();
+  }, [appState]);
+
+  useEffect(() => {
+    if (Object.keys(algos).length === 0 || !program) return;
+    methods.edit(program);
+    methods.run();
+  }, [algos]);
+
+  useEffect(() => {
+    if (appState !== AppState.DEBUG_READY || !step || !algoName) return;
+
+    const stepStrings = step.split(".");
+    const steps: number[] = stepStrings.map((stepStr, idx) =>
+      parseStep(stepStr, idx),
     );
-  }
-}
+    const bpName = `${steps} @ ${algoName}`;
+    const duplicated = breakpoints.some(
+      ({ name }: { name: string }) => name === bpName,
+    );
+
+    const valid = algos.hasOwnProperty(algoName);
+    if (valid && !duplicated)
+      methods.addBreak({
+        type: BreakpointType.Spec,
+        fid: algos[algoName],
+        name: bpName,
+        steps: steps,
+        enabled: true,
+      });
+    else if (duplicated) toast.warning(`Breakpoint already set: ${bpName}`);
+    else toast.warning(`Wrong algorithm name: ${algoName}`);
+
+    methods.specContinue();
+  }, [appState]);
+
+  return (
+    <div className="px-20">
+      <ConnectState />
+      <Toolbar />
+
+      <div className="grid grid-cols-3 gap-10">
+        <JSEditor />
+        <SpecViewer />
+        <StateViewer />
+      </div>
+    </div>
+  );
+};
 
 export default connector(DebuggerApp);
