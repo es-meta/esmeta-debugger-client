662d8d3860c942cf9db85a29f696585f255794eb
Minseok Choe <choems@outlook.com>
2024-12-22 21:44:03 +0900

    fix: remove mui

--- src/components/layout/Card.tsx
+++ src/components/layout/Card.tsx
@@ -3,31 +3,31 @@ import { type PropsWithChildren, useState, useRef } from "react";
 const UNIT = 8; // Define the height of one unit in rem.
 
 export default function Card({ children }: PropsWithChildren) {
-  const [height, setHeight] = useState(1); // Initial height in units.
+  const [height] = useState(8); // Initial height in units.
   const cardRef = useRef<HTMLDivElement | null>(null); // Ref for the card.
 
   // Handler for dragging.
-  const handleDrag = (event: React.MouseEvent) => {
-    if (cardRef.current) {
-      const cardTop = cardRef.current.getBoundingClientRect().top;
-      const newHeightInPx = event.clientY - cardTop; // Calculate new height.
-      const newHeightInUnits = Math.max(1, Math.round(newHeightInPx / (UNIT * 16))); // Convert px to unit (1rem = 16px).
-      setHeight(newHeightInUnits); // Update the height state.
-    }
-  };
+  // const handleDrag = (event: React.MouseEvent<HTMLDivElement>) => {
+  //   if (cardRef.current) {
+  //     const cardTop = cardRef.current.getBoundingClientRect().top;
+  //     const newHeightInPx = event.clientY - cardTop; // Calculate new height.
+  //     const newHeightInUnits = Math.max(1, Math.round(newHeightInPx / (UNIT * 16))); // Convert px to unit (1rem = 16px).
+  //     setHeight(newHeightInUnits); // Update the height state.
+  //   }
+  // };
 
-  // Prevent text selection while dragging.
-  const handleDragStart = (event: React.MouseEvent) => {
-    document.body.style.userSelect = "none";
-    document.addEventListener("mouseup", handleDragEnd);
-    document.addEventListener("mousemove", handleDrag);
-  };
+  // // Prevent text selection while dragging.
+  // const handleDragStart = () => {
+  //   document.body.style.userSelect = "none";
+  //   document.addEventListener("mouseup", handleDragEnd);
+  //   document.addEventListener("mousemove", handleDrag);
+  // };
 
-  const handleDragEnd = () => {
-    document.body.style.userSelect = "auto";
-    document.removeEventListener("mouseup", handleDragEnd);
-    document.removeEventListener("mousemove", handleDrag);
-  };
+  // const handleDragEnd = () => {
+  //   document.body.style.userSelect = "auto";
+  //   document.removeEventListener("mouseup", handleDragEnd);
+  //   document.removeEventListener("mousemove", handleDrag);
+  // };
 
   return (
     <div
@@ -38,7 +38,7 @@ export default function Card({ children }: PropsWithChildren) {
       {children}
       <div
         className="absolute bottom-0 left-0 right-0 h-2 cursor-row-resize bg-neutral-200"
-        onMouseDown={handleDragStart}
+        // onMouseDown={handleDragStart}
       />
     </div>
   );
