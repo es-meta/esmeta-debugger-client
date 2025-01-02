71f7f075dbffaf9d66c9559076bc76ea73d0f9f2
Minseok Choe <choems@outlook.com>
2025-01-03 00:05:35 +0900

    style: add es color

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{html,js,jsx,ts,tsx}"],
+  darkMode: 'class',
   theme: {
     fontWeight: {
       "100": "100",
@@ -16,7 +17,18 @@ export default {
 
     extend: {
       colors: {
-        es: '#ffa31a',
+        es: {
+          "50": '#fff6f0',
+          "100": '#ffedd9',
+          "200": '#ffdcb3',
+          "300": '#ffcb8d',
+          "400": '#ff9f44',
+          "500": '#ffa31a',
+          "600": '#ff9900',
+          "700": '#ff8a00',
+          "800": '#ff7a00',
+          "900": '#ff6b00',
+        }
       }
     },
   },
