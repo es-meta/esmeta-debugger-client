4798cdd891fb83f64bad1f1319011fa0d6cb8876
Minseok Choe <choems@outlook.com>
2025-01-03 16:54:48 +0900

    style: Header

--- src/components/custom/Header.tsx
+++ src/components/custom/Header.tsx
@@ -1,9 +1,14 @@
-import PlainLabel from "@/features/modal/PlainLabel";
-import { GithubIcon } from "lucide-react";
+import {
+  GITHUB_LINK_ESMETA,
+  GITHUB_LINK_ESMETA_DEBUGGER,
+} from "@/constants/constant";
+import tw from "tailwind-styled-components";
 
-function Header() {
+const A = tw.a`flex flex-row gap-1 items-center text-xs font-500 hover:bg-neutral-100 rounded-lg active:scale-90 transition-all cursor-pointer p-2`;
+
+export default function Header() {
   return (
-    <nav className="bg-white border-b border-b-neutral-300 bg-opacity-75 h-14 w-full text-sm lg:px-24 flex flex-row justify-between gap-3 items-center px-4">
+    <nav className="bg-white dark:text-white dark:bg-neutral-800 border-b border-b-neutral-300 dark:border-b-neutral-600 bg-opacity-75 h-14 w-full text-sm xl:px-24 flex flex-row justify-between gap-3 items-center px-4">
       <Left />
       <Right />
     </nav>
@@ -12,8 +17,8 @@ function Header() {
 
 function Left() {
   return (
-    <div className="flex flex-row gap-2 items-center justify-start">
-      <span className="font-800 text-lg flex flex-row items-center gap-1">
+    <div className="flex flex-row items-center justify-start">
+      <span className="font-800 text-base flex flex-row items-center gap-1">
         <img
           src="/icon.jpeg"
           aria-hidden
@@ -22,31 +27,45 @@ function Left() {
         />
         ESMeta
       </span>
-
-      <div className="text-neutral-500">Docs</div>
-
-      <div className="text-neutral-500">Playground</div>
-
-      <div className="text-neutral-500">GitHub</div>
+      &nbsp;
+      <span className="font-400 text-base flex flex-row items-center gap-1">
+        Double&nbsp;Debugger&nbsp;Playground
+      </span>
     </div>
   );
 }
 
 function Right() {
   return (
-    <div className="flex flex-row gap-8">
-        <PlainLabel>
-
-          <GithubIcon />
-
-          ESMeta
-          </PlainLabel>
-      <PlainLabel>
-          <GithubIcon />
-          ESMeta Debugger
-          </PlainLabel>
+    <div className="flex flex-row gap-2">
+      <A href={GITHUB_LINK_ESMETA} target="_blank" rel="noopener noreferrer">
+        <GitHubIcon />
+        ESMeta
+      </A>
+      <A
+        href={GITHUB_LINK_ESMETA_DEBUGGER}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <GitHubIcon />
+        ESMeta&nbsp;Debugger&nbsp;Client
+      </A>
     </div>
   );
 }
 
-export default Header;
+function GitHubIcon() {
+  return (
+    <svg
+      aria-disabled
+      fill="currentColor"
+      width={14}
+      height={14}
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <title>GitHub</title>
+      <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
+    </svg>
+  );
+}
