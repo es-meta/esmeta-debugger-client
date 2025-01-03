67003ad004ef5d7e989d2fa7d8d33a92963c8d22
Minseok Choe <choems@outlook.com>
2025-01-03 16:54:26 +0900

    style: combobox

--- src/components/combobox/MyCombobox.tsx
+++ src/components/combobox/MyCombobox.tsx
@@ -34,34 +34,31 @@ export default function MyCombobox({ values, value, onChange }: ComboProps) {
       onChange={value => onChange(value || "")}
     >
       <ComboboxInput
-        className="w-full p-2 border rounded-lg focus:border focus:border-blue-300 focus:outline-none"
+        className="font-mono text-sm w-full p-2 border focus:border focus:border-blue-300 focus:outline-none"
         onChange={event => setQuery(event.target.value)}
       />
-
-      {
-        <ComboboxOptions
-          transition
-          anchor="bottom"
-          className="z-[1] w-[var(--input-width)] origin-top border transition duration-200 ease-out empty:invisible data-[closed]:scale-95 data-[closed]:opacity-0 h-32 overflow-scroll bg-white rounded-lg"
-        >
-          {({ option: name }) => (
-            <ComboboxOption key={name} value={name} as={Fragment}>
-              {({ focus }) => (
-                <li
-                  className={twJoin(
-                    focus ? "bg-blue-200" : "bg-white",
-                    "p-2 cursor-pointer",
-                    "w-full",
-                  )}
-                >
-                  <CheckIcon className="hidden ui-selected:block" />
-                  {name}
-                </li>
-              )}
-            </ComboboxOption>
-          )}
-        </ComboboxOptions>
-      }
+      <ComboboxOptions
+        transition
+        anchor="bottom"
+        className="font-mono text-sm z-[101] shadow-lg w-[var(--input-width)] origin-top border transition duration-200 ease-out empty:invisible data-[closed]:scale-95 data-[closed]:opacity-0 h-96 overflow-scroll bg-white rounded-lg"
+      >
+        {({ option: name }) => (
+          <ComboboxOption key={name} value={name} as={Fragment}>
+            {({ focus }) => (
+              <div
+                className={twJoin(
+                  focus ? "bg-blue-200" : "bg-white",
+                  "p-2 cursor-pointer w-full",
+                )}
+                title={name}
+              >
+                <CheckIcon className="hidden ui-selected:block" />
+                {name}
+              </div>
+            )}
+          </ComboboxOption>
+        )}
+      </ComboboxOptions>
     </Combobox>
   );
 }
