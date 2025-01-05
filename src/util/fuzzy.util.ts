f5571a82e54ff8449f91d6a19758bf421e9810d4
Minseok Choe <choems@outlook.com>
2025-01-05 23:30:43 +0900

    feat: parametized fuzzy

--- src/util/fuzzy.util.ts
+++ src/util/fuzzy.util.ts
@@ -1,14 +1,18 @@
 import { Fzf } from "fzf";
 
-export const fuzzyFilter = (
-  source: string[],
+export const fuzzyFilter = <T>(
+  source: T[],
   query: string,
   threshold: number,
-): string[] => {
-  const fzf = new Fzf(source);
+  extractor: (item: T) => string
+): T[] => {
+  
+  const list = source.map((v) => ({ value: v, text: extractor(v) }));
+
+  const fzf = new Fzf(list, { selector: (item) => item.text });
   return fzf
     .find(query)
     .filter(r => r.score > threshold)
     .sort((a, b) => b.score - a.score)
-    .map(r => r.item);
+    .map(r => r.item.value);
 };
