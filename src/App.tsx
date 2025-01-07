33426e0619c76e602d3c2011c4b436d7e3a498a7
Minseok Choe <choems@outlook.com>
2025-01-07 20:18:17 +0900

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import { IS_DEBUG } from "./constants/constant";
 import CookiePopup from "./CookiePopup";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import WelcomeModal from "./features/docs/welcome/Welcome";
 
 const queryClient = new QueryClient();
 
@@ -17,11 +18,12 @@ export default function App() {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="min-h-dvh bg-neutral-100 dark:bg-neutral-800 pb-16">
-        <ToastContainer autoClose={3000} transition={Slide} />
+      <div className="min-h-dvh bg-neutral-100 dark:bg-neutral-800 pb-16 h-full flex flex-col">
         <Header />
         <DebuggerApp />
       </div>
+      <ToastContainer autoClose={3000} transition={Slide} />
+      <WelcomeModal />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
