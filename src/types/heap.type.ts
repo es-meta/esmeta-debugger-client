c9aa9a5731c7f46dbdb2f5ac61e04ba0762d2dba
Minseok Choe <choems@outlook.com>
2025-01-07 20:23:09 +0900

    type: heap

---
+++ src/types/heap.type.ts
@@ -0,0 +1,30 @@
+export interface HeapObjRecord {
+  type: "RecordObj";
+  tname: string;
+  map: Record<string, string>;
+  stringform: string;
+}
+
+export interface HeapObjList {
+  type: "ListObj";
+  values: Record<string, string>;
+  stringform: string;
+}
+
+export interface HeapObjMap {
+  type: "MapObj";
+  map: Record<string, string>;
+  stringform: string;
+}
+
+export interface HeapObjYet {
+  type: "YetObj";
+  tname: Record<string, string>;
+  stringform: string;
+}
+
+// types for ir state
+// beautified addr and value
+export type HeapObjs = HeapObjRecord | HeapObjList | HeapObjMap | HeapObjYet;
+
+export type Heap = { [addr: string]: HeapObjs };
