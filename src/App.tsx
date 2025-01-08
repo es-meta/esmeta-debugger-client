a89d30567ed1aa929a32204842f8cb1431d83fb8
Minseok Choe <choems@outlook.com>
2025-01-08 13:10:22 +0900

    remove: tanstack

--- src/App.tsx
+++ src/App.tsx
@@ -7,25 +7,19 @@ import Header from "@/components/custom/Header";
 import DebuggerApp from "@/features/debugger-app/DebuggerApp";
 import { IS_DEBUG } from "./constants/constant";
 import CookiePopup from "./CookiePopup";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import WelcomeModal from "./features/docs/welcome/Welcome";
-
-const queryClient = new QueryClient();
 
 export default function App() {
   useAppInitializer();
 
-  return (
-    <QueryClientProvider client={queryClient}>
+  return (<>
       <div className="min-h-dvh bg-neutral-100 dark:bg-neutral-800 pb-8 h-full flex flex-col">
         <Header />
         <DebuggerApp />
       </div>
-      <ToastContainer autoClose={3000} transition={Slide} />
+      <ToastContainer autoClose={1000} transition={Slide} />
       <WelcomeModal />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    </>
   );
 }
 
