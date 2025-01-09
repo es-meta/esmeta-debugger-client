7e4eb68e4413df2f56d0f82a7a0a40a4f8a9a075
Minseok Choe <choems@outlook.com>
2025-01-09 14:37:27 +0900

    fix: client state

--- src/features/state/heap/Address.tsx
+++ src/features/state/heap/Address.tsx
@@ -5,60 +5,101 @@ import ObjectView from "./ObjViewer";
 import {
   ChevronDownIcon,
   ChevronUpIcon,
-  CopyIcon,
   RewindIcon,
+  SearchIcon,
 } from "lucide-react";
 import { backToProvenance } from "@/store/reducers/Debugger";
-import CopyButton from "@/components/button/CopyButton";
+import { chooseStateViewer, setHeapViewerAddr } from "@/store/reducers/Client";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
 
 interface Props {
   address: `${string}`;
-  initialFold?: boolean;
+  singleMode?: boolean;
 }
 
-export default function Address({ address, initialFold }: Props) {
+
+
+export function ProvinenceButton({ address }: {
+  address?: string;
+}) {
+  const dispatch = useDispatch();
+  
+  return (
+    address &&
+    address.startsWith("#") &&
+    !Number.isNaN(Number(address.substring(1))) && (
+      <Tooltip><TooltipTrigger
+        className=" text-blue-400 inline cursor-pointer hover:text-blue-600 active:scale-75 transition-all"
+      onClick={() => {
+        dispatch(backToProvenance(address));
+      }}>
+        <RewindIcon size={16} className="inline" />
+      </TooltipTrigger>
+      <TooltipContent>
+      Go back to provenance
+    </TooltipContent>
+      </Tooltip>
+    )
+  );
+}
+
+function InspectInHaepViewer({ address }: { address: string }) {
   const dispatch = useDispatch();
 
+  return <Tooltip><TooltipTrigger
+        className="text-es-500 inline cursor-pointer hover:text-es-900 active:scale-75 transition-all"
+        content={address}
+        onClick={() => {
+          dispatch(setHeapViewerAddr(address));
+          dispatch(chooseStateViewer("heap"));
+        }}>
+        <SearchIcon size={16} /></TooltipTrigger>
+   <TooltipContent>
+     <p>Inspect in heap viewer</p>
+      </TooltipContent>
+      </Tooltip>
+};
+
+export default function Address({ address, singleMode }: Props) {
+
   const [fold, setFold] = useState(
-    initialFold === undefined ? false : initialFold,
+    singleMode === undefined ? false : singleMode,
   );
   const obj = useSelector((s: ReduxState) => s.irState.heap[address]);
 
   return (
     <div className="flex flex-col size-full">
-      <span className="flex flex-row items-center justify-center break-all font-mono">
+      {!singleMode && (
+      <span
+        className={
+          singleMode
+            ? "p-2 flex flex-row grow items-center justify-between break-all font-mono w-full gap-2"
+            : "flex flex-row items-center justify-center break-all font-mono w-full gap-2"
+        }
+      >
         {address}
 
-        {address &&
-          address.startsWith("#") &&
-          !Number.isNaN(Number(address.substring(1))) && (
-            <a
-              onClick={() => {
-                dispatch(backToProvenance(address));
-              }}
-              className="text-blue-400 inline cursor-pointer hover:text-blue-600 active:scale-50 transition-all"
-            >
-              <RewindIcon size={16} className="inline" />
-            </a>
-          )}
-        <CopyButton
-          className="text-es-500 inline cursor-pointer hover:text-es-900 active:scale-50 transition-all"
-          content={address}
-        >
-          <CopyIcon size={16} />
-        </CopyButton>
-        <a
-          className="inline cursor-pointer text-es-500 hover:text-es-900 active:scale-50 transition-all"
-          onClick={() => setFold(f => !f)}
-        >
-          {" "}
-          {fold ? (
-            <ChevronUpIcon size={16} className="inline" />
-          ) : (
-            <ChevronDownIcon size={16} className="inline" />
-          )}
-        </a>
+        <div className="flex flex-row items-center gap-1">
+          <ProvinenceButton address={address} />
+          <InspectInHaepViewer address={address} />
+          <a
+            className="inline cursor-pointer text-es-500 hover:text-es-900 active:scale-75 transition-all"
+            onClick={() => setFold(f => !f)}
+          >
+            {fold ? (
+              <ChevronUpIcon size={16} className="inline" />
+            ) : (
+              <ChevronDownIcon size={16} className="inline" />
+            )}
+              </a>
+        </div>
       </span>
+          )}
       {fold && (obj ? <ObjectView obj={obj} /> : <div>Not found</div>)}
     </div>
   );
