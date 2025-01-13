951200165b8b2d56580cd65376cc396cff006d75
Minseok Choe <choems@outlook.com>
2025-01-13 14:47:19 +0900

    fix: minor

--- src/components/label/PlainLabel.tsx
+++ src/components/label/PlainLabel.tsx
@@ -1,18 +1,20 @@
 import type { ReactNode } from "react";
-import { twJoin } from "tailwind-merge";
+import { twMerge } from "tailwind-merge";
 
 interface Props {
   children: ReactNode;
+  className?: string;
 }
 
-export default function PlainLabel({ children }: Props) {
+export default function PlainLabel({ children, className }: Props) {
   return (
     <div
-      className={twJoin(
+      className={twMerge(
         "flex flex-row items-center gap-[2px] px-1 py-1",
         "transition-all",
         "[&>svg]:size-4",
         "uppercase text-xs font-500 text-neutral-600",
+        className,
       )}
     >
       {children}
