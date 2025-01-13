af8fbbb3c9199125c332dbbfcc7c5b71a4659cac
Minseok Choe <choems@outlook.com>
2025-01-13 19:03:21 +0900

    feat: internal stat viewer for debugging

---
+++ src/features/state/internal/InternalStatViewer.tsx
@@ -0,0 +1,13 @@
+import { ReduxState } from "@/store";
+import { useSelector } from "react-redux";
+
+export default function InternalStatViewer() {
+  const debugString = useSelector((state: ReduxState) => state.stats.debugString);
+
+  return (
+    <div>
+      <h1>Internal Stat Viewer</h1>
+      <pre className="whitespace-pre-line">{debugString}</pre>
+    </div>
+  );
+};
