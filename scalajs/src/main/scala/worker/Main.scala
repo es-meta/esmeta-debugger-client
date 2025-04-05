e639d4cae8700035883ea5d198c04b6efc1ece1c
Minseok Choe <choems@outlook.com>
2025-04-05 15:13:44 +0900

    fixup

---
+++ scalajs/src/main/scala/worker/Main.scala
@@ -0,0 +1,64 @@
+package worker
+
+import org.scalajs.dom
+import scala.scalajs.js.annotation.*
+// import esmeta.web.WebServer
+
+// @JSExportTopLevel("HelloWorld")
+// object HelloWorld {
+//   @JSExport
+//   def sayHello(): Unit = {
+//     ESMetaSpec.doSomethingAsync()
+//   }
+
+
+  // @JSExport
+  // def draw(): Unit = {
+  //   dom.document.querySelector("#app").innerHTML = s"""
+  //   <div>
+  //     <a href="https://vitejs.dev" target="_blank">
+  //       <img src="/vite.svg" class="logo" alt="Vite logo" />
+  //     </a>
+  //     <h1>Hello Scala.js!</h1>
+  //     <p class="read-the-docs">
+  //       Click the button to parse next script:
+  //     </p>
+  //     <pre class="code">
+  //     ${ESMetaSpec.targetJSScript}
+  //     </pre>
+  //     <div class="card">
+  //       <button id="counter" type="button">Lettttsss gogo</button>
+  //       <p></p>
+  //     </div>
+  //   </div>
+  // """;
+  //   dom.document.querySelector("#counter").addEventListener("click", (_) => HelloWorld.sayHello())
+  // }
+
+// }
+
+
+
+@main
+def main(): Unit = {
+  // HelloWorld.sayHello()
+//   dom.document.querySelector("#app").innerHTML = s"""
+//   <div>
+//     <a href="https://vitejs.dev" target="_blank">
+//       <img src="/vite.svg" class="logo" alt="Vite logo" />
+//     </a>
+//     <h1>Hello Scala.js!</h1>
+//     <p class="read-the-docs">
+//       Click the button to parse next script:
+//     </p>
+//     <pre class="code">
+//     ${ESMetaSpec.targetJSScript}
+//     </pre>
+//     <div class="card">
+//       <button id="counter" type="button">gogo</button>
+//       <p></p>
+//     </div>
+//   </div>
+// """;
+//   dom.document.querySelector("#counter").addEventListener("click", (_) => HelloWorld.sayHello())
+}
