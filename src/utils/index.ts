124b016249bde780c7cd7d46cdf2b23b97dfc85a
Minseok Choe <choems@outlook.com>
2025-05-20 13:35:19 +0900

    feat: utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,6 @@
 export * from "./counter.utils";
 export * from "./ecmarkdown.utils";
+export * as es from "./es.utils";
 export * from "./fuzzy.utils";
 export * from "./logger.utils";
 export * from "./query.utils";
--- src/utils/style.utils.ts
+++ src/utils/style.utils.ts
@@ -1,1 +1,5 @@
 export { cn } from "@/lib/utils";
+
+export function asDataAttribute(boolean: boolean): true | undefined {
+  return boolean ? true : undefined;
+}
