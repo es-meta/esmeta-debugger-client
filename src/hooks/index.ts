f2cf1c723916b541bf43a5fe0c0ce2ea73c629a9
Minseok Choe <choems@outlook.com>
2025-05-20 13:34:47 +0900

    feat: hooks

--- src/hooks/index.ts
+++
@@ -1,7 +0,0 @@
-// hooks.ts (new file)
-import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
-import type { ReduxState, AppDispatch } from "@/store";
-export { shallowEqual } from "react-redux";
-
-export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector: TypedUseSelectorHook<ReduxState> = useSelector;
