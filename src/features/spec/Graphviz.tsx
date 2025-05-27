a03f2976ab5d68cddab9cf69bfb9ad918cfe2212
Minseok Choe <choems@outlook.com>
2025-05-27 16:10:44 +0900

    fix: debounce

--- src/features/spec/Graphviz.tsx
+++ src/features/spec/Graphviz.tsx
@@ -1,10 +1,11 @@
-import { useEffect, useId, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { graphviz, GraphvizOptions } from "d3-graphviz";
 import { twJoin } from "tailwind-merge";
 import { Loading } from "./Graphviz.load";
 import { useAtomValue } from "jotai";
 import { atoms } from "@/atoms";
 import { usePreferredColorScheme } from "@/hooks/use-preferred-color-scheme";
+import AwesomeDebouncePromise from 'awesome-debounce-promise';
 
 interface Props {
   dot: string;
@@ -15,15 +16,34 @@ const defaultOptions: GraphvizOptions = {
   zoom: true,
 };
 
-export default function Graphviz({ dot }: Props) {
+async function getNewDiv(dot: string): Promise<HTMLDivElement> {
+  return new Promise<HTMLDivElement>((resolve) => {
+    const div = document.createElement("div");
+
+    graphviz(div, defaultOptions).renderDot(dot, () => {
+      resolve(div);
+    });
+  });
+}
+
+const getNewDivDebounced = AwesomeDebouncePromise(getNewDiv, 100, { onlyResolvesLast : true });
+
+function GraphvizContent({ dot }: Props) {
   const theme = usePreferredColorScheme();
+  const ref = useRef<HTMLDivElement>(null);
   const [completed, setCompleted] = useState<string | null>(null);
-  const id = useId();
 
   useEffect(() => {
-    graphviz(`#${CSS.escape(id)}`, defaultOptions).renderDot(dot, () =>
-      setCompleted(dot),
-    );
+      (getNewDivDebounced(dot)).then(div => {
+        if (div && ref.current) {
+          ref.current.innerHTML = ""; // Clear previous content
+          ref.current.append(...div.childNodes);
+        }
+      }).then(() => {
+        setCompleted(dot);
+      }).finally(() => {
+        
+      });
   }, [dot]);
 
   const busy = useAtomValue(atoms.app.busyStateGetter);
@@ -33,7 +53,7 @@ export default function Graphviz({ dot }: Props) {
     <div className="relative [&>&>svg]:size-full size-full">
       {isLoading ? <Loading /> : null}
       <div
-        id={id}
+        ref={ref}
         className={twJoin(
           "size-full overflow-hidden [&>svg]:size-full transition-opacity duration-100",
           isLoading ? "opacity-90" : "",
@@ -42,4 +62,8 @@ export default function Graphviz({ dot }: Props) {
       />
     </div>
   );
+}
+
+export default function Graphviz({ dot }: Props) {
+  return <GraphvizContent dot={dot} />;
 }
